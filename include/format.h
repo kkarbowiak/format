c7fcc3dcc097816828f67a10f11f47f363aff137
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2023-10-06 19:59:13 +0200

    Simplify code

--- include/format.h
+++ include/format.h
@@ -8,13 +8,6 @@ auto operator""_fmt(char const * text, long unsigned int)
 {
     return [=](auto && ... ts)
     {
-        if constexpr (sizeof...(ts) == 0)
-        {
-            return std::string(text);
-        }
-        else
-        {
-            return std::vformat(text, std::make_format_args(ts...));
-        }
+        return std::vformat(text, std::make_format_args(ts...));
     };
 }
