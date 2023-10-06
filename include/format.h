9bed2c54582b1bca82acef6b20e51b860ebb1471
Krzysiek Karbowiak <krzysztof.karbowiak@interia.pl>
2023-10-06 19:55:17 +0200

    Reimplement using std::format

--- include/format.h
+++ include/format.h
@@ -4,17 +4,31 @@
 #include <format>
 
 
-auto operator""_fmt(char const * text, long unsigned int)
+template<std::size_t N>
+struct FormatHelper
+{
+    char p[N]{};
+
+    constexpr FormatHelper(char const (&pp)[N])
+    {
+        for (std::size_t i = 0; i < N; ++i)
+        {
+            p[i] = pp[i];
+        }
+    }
+};
+template<FormatHelper fh>
+auto operator""_fmt()
 {
     return [=](auto && ... ts)
     {
         if constexpr (sizeof...(ts) == 0)
         {
-            return std::string(text);
+            return std::string(fh.p);
         }
         else
         {
-            return std::vformat(text, std::make_format_args(ts...));
+            return std::format(fh.p, ts...);
         }
     };
 }
